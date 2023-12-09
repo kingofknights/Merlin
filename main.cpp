7db69be1b11374ae6b87504ce07cbd91d0b362ae
kingofknights <vikram.smvdu@gmail.com>
2023-12-09 18:41:46 +0530

    added cpp_weekly cmake

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "include/Merlin.hpp"
-int main(int argc_, const char** argv_) {
-	Merlin merlin;
-	merlin.run();
-	return 0;
+int main([[maybe_unused]] int argc_, [[maybe_unused]] const char **argv_)
+{
+  Merlin merlin;
+  merlin.run();
+  return 0;
 }
